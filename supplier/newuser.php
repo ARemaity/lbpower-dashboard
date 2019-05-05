92d4fcb4a1a784a54b5bf8410d6e5795517be2e6
ali rmaity <aliremaitybook@gmail.com>
2019-05-05 17:36:29 +0300

    update php supplier (firebase)

--- supplier/newuser.php
+++ supplier/newuser.php
@@ -1,62 +1,72 @@
-<html>
-<head>
-  <title>lbpower</title>
-  <script src="https://code.jquery.com/jquery-3.4.0.min.js"> </script>
-  <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-  <link rel="stylesheet" href="css/ulogin.css" />
-</head> 
-
-<body>
-<?php
-
-if(!isset($_SERVER['HTTP_REFERER']))
-{     echo '
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-<script>
-    window.location.replace("http://localhost/firebaseWebLogin/ulogin.html");</script>   
-    </body>
-    </html>
-        ';   }
-
-else if ( isset($_GET['id'])&&isset($_GET['id'])&&isset($_GET['id'])&&isset($_GET['id'])&&isset($_GET['id'])&&isset($_GET['id'])&&isset($_GET['id'])) {
-
-$uid=$_GET['id'];
-$_SESSION['id']=$uid;
-
-echo ' <div id="x" class="y-div">
-<button onclick="logout()">Logout</button>
-</div>
-';
-
-}
-else{
 
 
-echo "sorry bro";
 
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password ="";
+$dbname="id8992783_isd";
+$status=0;
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
 
-?>
 
-<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
-    <script src="js/auth.js"></script>
-    <script>
-    
-    
-    function logout() {
-            firebase.auth().signOut();
-            window.location.replace("http://localhost/firebaseWebLogin/ulogin.html");
-        }
-</script>
 
-</body>
-</html>
+
+
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+ 
+$PID=$_POST["uid"];
+  
+ $fname=  $_POST["fname"];
+
+  $lname= $_POST["lname"]; 
+ 
+ $phone= $_POST["phone"];
+  $city= $_POST["city"];
+ //$id=  $_POST["id"];
+ //DONE:get uid and send it throgh ajax 
+ //TODO:get PID From session and insert in to Client tbl
+
+ //...  
+//as example pid is 0 it should be taken from the session;
+$fk_supplier=1;
+ //
+
+ $street=  $_POST["street"];
+ $email=  $_POST["email"]; 
+ // $password= $_POST["password"];
+ 
+ 
+
+
+ $role=0;
+ $insert = mysqli_query($conn, " INSERT INTO person (role,fname,lname,city,street,phone,email)  VALUES ('" . $role . "','" . $fname . "','" . $lname ."','" . $city ."','" . $street ."','" . $phone . "','" . $email ."')");
+    $checkLastid = mysqli_query($conn, "SELECT PID FROM person ORDER BY PID DESC LIMIT 1 ");
+    $value = mysqli_fetch_object($checkLastid);
+     $last = (int)$value->PID;
+ $insertClient = mysqli_query($conn, " INSERT INTO client (id,PID,fkSupplier)  VALUES ('" . $PID . "','" . $last . "','" . $fk_supplier ."')");
+if($insert&&$insertClient){
+
+   echo "USER successfull inserted";
+
+}else{
+    echo "there is problem ,repeat the process if return contact support";
+}
+ 
+ 
+ ?>
+ 
+ 
+
+
+
+
+
